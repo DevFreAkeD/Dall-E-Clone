3726261a76082ab2f0197a8ae75b18b46907e0aa
Tushar Nagar <tusharrock146@gmail.com>
2024-06-07 09:12:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,12 @@ import { Home, CreatePost } from './pages';
 
 const App = () => (
   <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" height={40}/>
-      </Link>
+    <header className="w-full flex justify-between items-center bg-gray-100 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="px-2 w-28 object-contain" height={40}/>
+        </Link>
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#375cb0] text-white px-4 py-2 rounded-md">Create</Link>
+        <Link to="/create-post" className="font-inter font-medium bg-[#375cb0] text-white px-4 py-2 rounded-md">Create</Link>
     </header>
 
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
