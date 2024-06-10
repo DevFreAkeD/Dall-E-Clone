c71b4f00ab022fc0f109c08ccbdc766d65c70ef9
Tushar Nagar <tusharrock146@gmail.com>
2024-06-10 19:14:53 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -67,12 +67,12 @@ const Home = () => {
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
                 <RenderCards
-                  data=""
+                  data={searchedResults}
                   title="No Search Results Found"
                 />
               ) : (
                 <RenderCards
-                  data=""
+                  data={allPosts}
                   title="No Posts Yet"
                 />
               )}
