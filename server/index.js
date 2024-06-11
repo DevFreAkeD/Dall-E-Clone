edabe19e02e432fc2f4a151e29a22150a85be3ee
Tushar Nagar <tusharrock146@gmail.com>
2024-06-11 12:59:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect.js';
 
 dotenv.config();
 
@@ -13,7 +14,13 @@ app.get('/', async (req, res) => {
 });
 
 const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
     app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
+  }
+  catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
