b83b203cc14d3ba42cc233dcdd0ae0345acb37a3
Tushar Nagar <tusharrock146@gmail.com>
2024-06-11 21:22:31 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -4,3 +4,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  
+  const openai = new OpenAIApi(configuration);
+  
+  router.route('/').get((req, res) => {
+    req.send('Hello from DALL-E!');
+  });
+  
+export default router;
