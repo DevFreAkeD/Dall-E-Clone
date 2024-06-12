3233864e029b9cf6b462c53216bf8eb4d2d2b524
Tushar Nagar <tusharrock146@gmail.com>
2024-06-12 12:03:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -9,16 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/post', postRoutes);
+
 app.get('/', async (req, res) => {
   res.send('Hello from DALL-E!');
 });
 
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
+    await connectDB(process.env.MONGODB_URL); // Ensure the database connection is awaited
     app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
   }
 };
