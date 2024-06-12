2e341e2d913787678bcb1380ff8101770fe7aea8
Tushar Nagar <tusharrock146@gmail.com>
2024-06-12 12:02:59 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -62,7 +62,7 @@ const CreatePost = () => {
           />
         </div>
 
-        <div className="mt-5 relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+        <div className="mt-5 relative bg-gray-100 border border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
           <img
             src={preview}
             alt="preview"
