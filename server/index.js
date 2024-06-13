a608d37fa11e08138665d13fd9f7e50cb05ed95d
Tushar Nagar <32740788+DevFreAkeD@users.noreply.github.com>
2024-06-13 12:50:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import dalleRoutes from './routes/dalleRoutes.js';
-import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/dalle', dalleRoutes);
-app.use('/api/v1/post', postRoutes);
 
 app.get('/', async (req, res) => {
   res.send('Hello from DALL-E!');
