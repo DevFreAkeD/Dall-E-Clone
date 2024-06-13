30a4322efb482aaf43759d2734c5a208f4655d9b
Tushar Nagar <tusharrock146@gmail.com>
2024-06-13 12:49:36 +0530

    Update index.js

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png'
 import logo from './dall_e_logo.png'
 import preview from './preview.png'
+import poster from './dall-e-2-poster.png'
 
 export {
   download,
   logo,
   preview,
+  poster,
 };
