f688a4c7f93c654ae96ffb0b34a9414ccf45b2ea
Tushar Nagar <tusharrock146@gmail.com>
2024-06-13 13:21:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,12 @@ const App = () => (
           <img src={logo} alt="logo" className="px-2 w-28 object-contain" height={40}/>
         </Link>
 
-        <Link to="/create-post" className="font-inter font-medium bg-[#375cb0] text-white px-4 py-2 rounded-md">Create</Link>
+        <Link to="/create-post" className="flex font-inter font-medium bg-[#375cb0] text-white px-4 py-2 text-sm w-40 text-center rounded-full text-nowrap min-h-button items-center justify-center gap-[0.3em]">
+          Try DALL-E
+          <svg width="0.625rem" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
+          </svg>
+        </Link>
     </header>
 
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
