dfb6e050325d3118a9c361b9168e969cbf34f316
Tushar Nagar <tusharrock146@gmail.com>
2024-06-13 13:22:12 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { poster } from '../assets';
 
 const Home = () => {
-
   return (
     <section className='max-w-7xl mx-auto'>
       <div className='relative mb-xl m:mb-2xl'>
@@ -11,7 +11,7 @@ const Home = () => {
               DALL-E
             </h1>
             <div className="mt-xs m:grid m:grid-cols-12">
-              <p className="col-span-12 m:col-span-8 m:col-start-3 text-subhead justify-center text-[18px] text-cta w-full max-w-xs sm:max-w-sm md:max-w-xs lg:max-w-lg">
+              <p className="col-span-12 m:col-span-8 m:col-start-3 text-subhead justify-center text-[18px] text-cta w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
                 DALL-E is an AI system that can create realistic images and art from a description in natural language.
               </p>
             </div>
@@ -26,10 +26,16 @@ const Home = () => {
             >
               Try DALL-E
               <svg width="0.625rem" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M1 9L9 1M9 1H2.5M9 1V7.22222" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
               </svg>
               <span className="sr-only">(opens in a new window)</span>
             </button>
+          </div>
+
+          <div className="flex justify-center mt-10">
+            <div className="w-full max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-2xl bg-gray-100 rounded-lg">
+              <img src={poster} alt="Poster" className="w-full rounded-lg" />
+            </div>
           </div>
 
         </div>
@@ -38,4 +44,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
