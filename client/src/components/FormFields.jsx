e1a07cde240c1820881186c2609679560f5acce9
Tushar Nagar <tusharrock146@gmail.com>
2024-06-13 13:22:01 +0530

    Update FormFields.jsx

--- client/src/components/FormFields.jsx
+++ client/src/components/FormFields.jsx
@@ -12,6 +12,12 @@ const FormFields = ({
 }) => (
   <div>
     <div className="flex items-center gap-2 mb-2">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-900"
+      >
+        {labelName}
+      </label>
       {isSurpriseMe && (
         <button
           type="button"
