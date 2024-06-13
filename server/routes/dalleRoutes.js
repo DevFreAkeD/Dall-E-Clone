ee0af070016bbce7dc4b56f271b29174f57873ea
Tushar Nagar <tusharrock146@gmail.com>
2024-06-13 09:18:42 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,18 +1,43 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // Ensure your environment variable is correctly set
+});
+
+router.route('/').get((req, res) => {
+  res.send('Hello from DALL-E!');
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+      const { prompt } = req.body;
   
-  const openai = new OpenAIApi(configuration);
+      const aiResponse = await openai.images.generate({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });
   
-  router.route('/').get((req, res) => {
-    req.send('Hello from DALL-E!');
+      const image = aiResponse.data.data[0].b64_json;
+      res.status(200).json({ photo: image });
+    } catch (error) {
+      console.error(error);
+
+      let errorMessage = 'Something went wrong';
+      if (error.response) {
+        if (error.response.data && error.response.data.error) {
+          errorMessage = error.response.data.error.message;
+        }
+      }
+      res.status(500).send(errorMessage);
+    }
   });
   
 export default router;
